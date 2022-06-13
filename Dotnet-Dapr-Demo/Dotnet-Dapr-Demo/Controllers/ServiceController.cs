a38c67bf3ed37431de051aac19c1e1baba136453
jiajunchen <jiajunchen@kaifa.cn>
2022-06-13 10:30:48 +0800

    update

---
+++ Dotnet-Dapr-Demo/Dotnet-Dapr-Demo/Controllers/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnet_Dapr_Demo.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("This is Service");
+        }
+    }
+}
